7f23dc575a72eed68c33c460e57846f6583c50fa
Luís Adolfo Alves de Araújo <76081629+LuisAdolfoAlves@users.noreply.github.com>
2021-02-22 23:23:58 -0300

    Aprendendo Desenvolvimento Web

---
+++ JavaScript/ex_06.js
@@ -0,0 +1,6 @@
+var vel = 59.8
+console.log(`A velcidade do seu carro é ${vel} km/h`)
+if (vel > 60) {
+    console.log("Você ultrapassou a velocidade permitida. Multado!")
+}
+console.log('Dirija sempre usando cinto de segurança!')
